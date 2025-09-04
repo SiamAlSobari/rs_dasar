e3ba0e4d19d525cd994aac82e0a9eec8e2d24505
SiamAlSobari <siamqueen52@gmail.com>
2025-09-05 02:00:22 +0700

    feat: add hallo world

---
+++ src/hallo.rs
@@ -0,0 +1,8 @@
+pub fn sapa_dunia() {
+    println!("Hallo world");
+    println!("Hallo world")
+}
+
+// fn private_sapa_dunia() {
+//     println!("Hallo world private fn")
+// }
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod hallo;
+
+fn main() {
+    hallo::sapa_dunia();
+    hallo::sapa_dunia();
+}
+
+#[test]
+fn unit_test() {
+    println!("Ini dari unit_test")
+}
